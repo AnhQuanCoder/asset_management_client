eb7a1d4828ad586ad13788db9ba74d88ea1a51aa
AnhQuanCoder <thanhquanait@gmail.com>
2025-08-27 16:29:41 +0700

    [FEAT] Cấu hình tailwind css cho dự án

---
+++ src/layouts/layout-default.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "styles/index.css";
+
+const LayoutDefault = () => {
+  return (
+    <>
+      Hehe
+    </>
+  )
+}
+
+export default React.memo(LayoutDefault);
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+const LayoutDefault = React.lazy(() => import("layouts/layout-default"));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <LayoutDefault />
   </StrictMode>,
 )
